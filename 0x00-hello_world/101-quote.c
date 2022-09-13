02a99ec45bd33fbd6b4b508030e38913765639f5
Chronicle Bilham <chrobilham@gmail.com>
2022-09-13 01:17:37 -0700

    Hello world project on c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry pointof program
+ *
+ * Return: will be a 1
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
+	return (1);
+}
