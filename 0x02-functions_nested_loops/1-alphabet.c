48746bd3741b41f202d5f593cdf5a1e194befe5f
Chronicle Bilham <chrobilhamgit add zero_daygit commit -m 'Fixing syntax'git pushcleargit -agit add .git config --global user.email chrobilham>
2022-09-17 20:04:27 -0700

    first time

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - print lowercase alphabet
+ * Return: 0
+ */
+
+void print_alphabet(void)
+
+{
+	char alpha;
+
+	alpha = 'a';
+	
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n')
+
+}
