bd7a326946f233f10b8256e3bbf97c4c641698ac
Chronicle Bilham <chrobilhamgit add zero_daygit commit -m 'Fixing syntax'git pushcleargit -agit add .git config --global user.email chrobilham>
2022-09-23 19:44:15 -0700

    edit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,16 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- *  * _strcat - concatenates two strings
- *   * @dest: string to append to
- *    * @src: string to add
- *     *
- *      * Return: a pointer to the resulting string
+ *  * _strcat - concatenate two strings
+ *   * @dest: string to be appended to
+ *    * @src: string to append
+ *     * Return: concatenated string
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
-
-	i = 0;
-	j = 0;
+	int i = 0;
+	int j = 0;
 
 	while (dest[i] != '\0')
 		i++;
@@ -20,10 +18,9 @@ char *_strcat(char *dest, char *src)
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-		j++;
 		i++;
+		j++;
 	}
-
 	dest[i] = '\0';
 
 	return (dest);
